50f7b8fb37587469fb208f8d3e5aa959a84d4ecc
wnghdcjfe <jhc9639@naver.com>
2025-03-04 10:05:40 +0900

    프롬프트 변경 closed https://github.com/PENEKhun/itdoc-poc/issues/9

--- script/prompt/index.js
+++ script/prompt/index.js
@@ -1,6 +1,6 @@
 function getCdocprompt(content) {
     return ` 
-    다음의 테스트내용을 기반으로 다음의 인터페이스를 갖춘 함수를 출력해주세요. 오로지 자바스크립트파일로만 떨어져야 합니다. 
+    다음의 테스트내용을 기반으로 다음의 인터페이스를 갖춘 함수를 출력해주세요. 오로지 자바스크립트파일로만 떨어져야 하며 코드에 대한 설명은 하지 않습니다.
     테스트내용:
     ${content}
     인터페이스:
@@ -8,18 +8,18 @@ function getCdocprompt(content) {
      - 테스트 케이스 함수: itDoc은 각 세부 테스트 시나리오를 기술하며, 테스트 실행을 위한 설정을 포함합니다.
      - 테스트 실행: 각 테스트는 apiDoc.test()를 통해 실행되며, 메서드체이닝으로 withRequestBody()와 expectStatus()로 요청본문과 응답상태값 등을 수행합니다.  
     
-    함수예시: 
+    함수예시:  
     describeAPI(
       HttpMethod.POST,
       '/signup',
       {
         name: '회원가입 API',
         tag: 'Auth',
-        summary: '사용자로부터 아이디와 패스워드를 받아 회원가입을 수행합니다.',
+        summary: '사용자로 부터 아이디와 패스워드를 받아 회원가입을 수행합니다.',
       },
       targetApp,
-      function (apiDoc) {
-        itDoc('회원가입 성공', async function () {
+      (apiDoc) => {
+        itDoc('회원가입 성공', async () => {
           await apiDoc
             .test()
             .withRequestBody({
@@ -29,8 +29,8 @@ function getCdocprompt(content) {
             // .withPrettyPrint()
             .expectStatus(HttpStatus.CREATED);
         });
-    
-        itDoc('아이디를 입력하지 않으면 회원가입 실패한다.', async function () {
+
+        itDoc('아이디를 입력하지 않으면 회원가입 실패한다.', async () => {
           await apiDoc
             .test()
             .withRequestBody({
@@ -38,11 +38,11 @@ function getCdocprompt(content) {
             })
             .expectStatus(HttpStatus.BAD_REQUEST)
             .expectResponseBody({
-              error: 'username is required',
+              "error": field('에러 메세지', 'username is required')
             });
         });
-    
-        itDoc('패스워드가 8자 이하면 회원가입 실패한다.', async function () {
+
+        itDoc('패스워드가 8자 이하면 회원가입 실패한다.', async () => {
           await apiDoc
             .test()
             .withRequestBody({
@@ -51,12 +51,48 @@ function getCdocprompt(content) {
             })
             .expectStatus(HttpStatus.BAD_REQUEST)
             .expectResponseBody({
-              error: 'password must be at least 8 characters',
+              "error": field('에러 메세지', 'password must be at least 8 characters')
             });
+        });
+      },
+    );
+
+    describeAPI(
+      HttpMethod.GET,
+      '/users/{userId}',
+      {
+        name: '사용자 조회 API',
+        tag: 'User',
+        summary: '사용자 ID를 받아 사용자 정보를 반환합니다.',
+      },
+      targetApp,
+      (apiDoc) => {
+        itDoc('유효한 사용자 ID가 주어지면 200 응답을 반환한다.', async () => {
+          await apiDoc
+            .test()
+            .withPathParams({
+              userId: field('유효한 사용자 ID', 'penek'),
+            })
+            .expectStatus(HttpStatus.OK)
+            .expectResponseBody({
+              userId: field('유저 ID', 'penek'),
+              username: field('유저 이름', 'hun'),
+              email: field('유저 이메일', 'penekhun@gmail.com'),
+              friends: field('유저의 친구', ['zagabi', 'json']),
+            });
+        });
+
+        itDoc('존재하지 않는 사용자 ID가 주어지면 404 응답을 반환한다.', async () => {
+          await apiDoc
+            .test()
+            .withPathParams({
+              userId: field('존재하지 않는 사용자 ID', 'invalid-user-id'),
+            })
+            .expectStatus(HttpStatus.NOT_FOUND);
         });
       }
     ); 
-    `
+`
 }
   
 module.exports = { getCdocprompt };
